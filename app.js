398378ea02b25f31bebed93fafab9a652beed1e6
AS-2K20 <amudhan.psgcas@gmail.com>
2020-06-18 13:51:59 +0530

    Updated Reject Function

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var evenNumbers = numberArray.filter(function isEven(numberArray){
 console.log(evenNumbers);
 
 var newArray = numberArray.filter(function isPrime(numberArray){
-    if(numberArray === 2){
+    if(numberArray === 1){
         return false;
     }
     else{
@@ -42,20 +42,43 @@ console.log(newArray);
 //Function #4: Reject
 
 var numberArray = [12,324,213,4,2,3,45,4234];
-var newArray = _.reject(numberArray, function nonPrime(numberArray){
-    if(numberArray === 2){
-        return true;
-    }
-    else{
-    for (var i = 2; i <= Math.sqrt(numberArray); i++) {           
-        if (numberArray % i === 0)   { 
-            return true;            
-        }        
-    }
-        return false;     
-       }
 
-});
+function reject(number_Array) {
+    var newArray = number_Array.filter(function non_prime_check(number_Array){
+        if(number_Array === 1){
+            return true;
+        }
+        else{
+        for (var i = 2; i <= Math.sqrt(number_Array); i++) {           
+            if (number_Array % i === 0)   { 
+                return true;            
+            }        
+        }
+        return false;     
+            }
+    });
+    return newArray;
+}
+    var not_prime = reject(numberArray,function nonPrime(number){
+        return number;
+    });
+    console.log(not_prime);
+
+
+// var newArray = _.reject(numberArray, function nonPrime(numberArray){
+//     if(numberArray === 2){
+//         return true;
+//     }
+//     else{
+//     for (var i = 2; i <= Math.sqrt(numberArray); i++) {           
+//         if (numberArray % i === 0)   { 
+//             return true;            
+//         }        
+//     }
+//         return false;     
+//        }
+
+// });
 
 console.log(newArray);
 
